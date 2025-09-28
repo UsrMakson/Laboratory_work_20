6f7a39327001312f2ac75b6bed50219a1d5b107f
Maksim <166782494+UsrMakson@users.noreply.github.com>
2025-09-28 16:35:28 +0500

    base
    280920251635 00

---
+++ 3_4/analysis.h
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+#include <vector>
+#include <string>
+
+class Analysis
+{
+public:
+	static int get_cin_n();
+	static int get_cin_height();
+	static int get_file_height();
+};
+=======
+#include <vector>
+#include <string>
+
+class Analysis
+{
+public:
+	static int get_cin_n();
+	static int get_cin_height();
+	static int get_file_height();
+};
+>>>>>>> 7daa9e4afda4d958361cd9196d3aa983099d1102
